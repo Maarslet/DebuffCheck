9bd8c2e792ea55d5cac03fca34447bb14b7c8fd7
Maarslet <12084712+Maarslet@users.noreply.github.com>
2020-09-22 23:16:27 +0200

    Update debuffAnalyser.js

--- debuffAnalyser.js
+++ debuffAnalyser.js
@@ -1,7 +1,7 @@
 function processInput() {
 
 var input = document.querySelector("#input").value;
-document.getElementById("demo").innerHTML = 5 + 6;
+document.getElementById("page").innerHTML = 5 + 7;
   
   
 //document.write(input);
