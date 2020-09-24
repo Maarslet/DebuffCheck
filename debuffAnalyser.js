1ab6532fd9ab426922daa065027d23334d78ec11
Maarslet <12084712+Maarslet@users.noreply.github.com>
2020-09-24 11:33:03 +0200

    Update debuffAnalyser.js

--- debuffAnalyser.js
+++ debuffAnalyser.js
@@ -16,23 +16,14 @@ function processInput() {
   var baseURL = "https://classic.warcraftlogs.com:443/v1/report";
   var filter = "&filter=encounterID%20%21%3D%200%20";
   try {
-  //var fightData = axios.get(baseURL + "/fights/" + logID + "?" + API.slice(1,API.length));
-  //fightData = fightData.getContentText();
-  //fightData = fightData.json();
   var fightData = new XMLHttpRequest();
-      fightData.open("Get", baseURL + "/fights/" + logID + "?" + API.slice(1,API.length), false);//.then(response => {response.data});
-      fightData.send(null);/*
-    fightData.onload = function () {
-    console.log(fightData);
-    console.log(fightData.readyState);
-    console.log(fightData.responseURL);
-    fightData = JSON.parse(fightData.response);
-    };
-     fightData.send(null);*/
+      fightData.open("Get", baseURL + "/fights/" + logID + "?" + API.slice(1,API.length), false);
+      fightData.send(null);
     console.log(fightData);
     console.log(fightData.readyState);
     console.log(fightData.response);
     fightData = JSON.parse(fightData.response);
+    console.log(fightData);
   document.getElementById("page").innerHTML = fightData; return
   document.getElementById("page").innerHTML = "Checkpoint 0";
   var debuffData = fetch(baseURL + "/events/debuffs/" + logID + "?start=0&end=100000000&hostility=1&wipes=2&filter=encounterID%21%3D0" + API);
