ed7a81e69cc12cb86785b45a9671c5c7edd0a5d2
Maarslet <12084712+Maarslet@users.noreply.github.com>
2020-09-24 09:44:49 +0200

    Update debuffAnalyser.js

--- debuffAnalyser.js
+++ debuffAnalyser.js
@@ -19,8 +19,8 @@ function processInput() {
   //var fightData = axios.get(baseURL + "/fights/" + logID + "?" + API.slice(1,API.length));
   //fightData = fightData.getContentText();
   //fightData = fightData.json();
-  var fightData = axios.get(baseURL + "/fights/" + logID + "?" + API.slice(1,API.length));
-    fightData = fightData.data;
+  var fightData = fetch(baseURL + "/fights/" + logID + "?" + API.slice(1,API.length));
+    //fightData = fightData.data;
     console.log(fightData);
   document.getElementById("page").innerHTML = fightData; return
   document.getElementById("page").innerHTML = "Checkpoint 0";
