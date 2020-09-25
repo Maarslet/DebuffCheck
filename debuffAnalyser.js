2ffdd42f01f8052327b5b5c9a7db4d5e7c9397e5
Maarslet <12084712+Maarslet@users.noreply.github.com>
2020-09-25 15:26:21 +0200

    Update debuffAnalyser.js

--- debuffAnalyser.js
+++ debuffAnalyser.js
@@ -143,7 +143,7 @@ function processInput() {
       debuffEdit[i].ability.name = "Faerie Fire";}
     if (debuffEdit[i].type == "removedebuff" && debuffEdit[i].timestamp == debuffEdit[i-1].timestamp && debuffEdit[i-1].type == "applydebuff") {
       console.log("Check 1")
-      for (j=i; j<=10; j++) {
+      for (j=i; j<=i+10; j++) {
         console.log("Check 2")
         if (debuffEdit[j].timestamp == debuffEdit[i-1].timestamp) {
           console.log("Check 3")
