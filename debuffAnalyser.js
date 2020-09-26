52c2cfb154a4adc944a4834b96861a7a5e3d1c10
Maarslet <12084712+Maarslet@users.noreply.github.com>
2020-09-26 20:26:23 +0200

    Update debuffAnalyser.js

--- debuffAnalyser.js
+++ debuffAnalyser.js
@@ -206,8 +206,8 @@ function processInput() {
       debuffOne = fightData.friendlies[friendIDs.indexOf(debuffEdit[idx].sourceID)].name + "'s " + debuffEdit[idx].ability.name;
     }
     catch (err) {
-      console.log(fightData.friendlyPets[petIDs.indexOf(debuffEdit[idx].sourceID)].owner)
-      //debuffOne = fightData.friendlies[friendIDs.indexOf(fightData.friendlyPets[petIDs.indexOf(debuffEdit[idx].sourceID)].owner)].name + "'s Pet's " + debuffEdit[idx].ability.name;
+      console.log(fightData.friendlyPets[petIDs.indexOf(debuffEdit[idx].sourceID)].petOwner)
+      debuffOne = fightData.friendlies[friendIDs.indexOf(fightData.friendlyPets[petIDs.indexOf(debuffEdit[idx].sourceID)].petOwner)].name + "'s Pet's " + debuffEdit[idx].ability.name;
       console.log(err)
       console.log(fightData.friendlies[friendIDs.indexOf(debuffEdit[idx].sourceID)])
       console.log(friendIDs.indexOf(debuffEdit[idx].sourceID))
@@ -218,8 +218,8 @@ function processInput() {
     debuffTwo = fightData.friendlies[friendIDs.indexOf(debuffEdit[idx+1].sourceID)].name + "'s " + debuffEdit[idx+1].ability.name;
     }
     catch (err) {
-      console.log(fightData.friendlyPets[petIDs.indexOf(debuffEdit[idx+1].sourceID)].owner)
-      //debuffTwo = fightData.friendlies[friendIDs.indexOf(fightData.friendlyPets[petIDs.indexOf(debuffEdit[idx+1].sourceID)].owner)].name + "'s Pet's " + debuffEdit[idx+1].ability.name;
+      console.log(fightData.friendlyPets[petIDs.indexOf(debuffEdit[idx+1].sourceID)].petOwner)
+      debuffTwo = fightData.friendlies[friendIDs.indexOf(fightData.friendlyPets[petIDs.indexOf(debuffEdit[idx+1].sourceID)].petOwner)].name + "'s Pet's " + debuffEdit[idx+1].ability.name;
       console.log(err)
       console.log(fightData.friendlies[friendIDs.indexOf(debuffEdit[idx+1].sourceID)])
       console.log(friendIDs.indexOf(debuffEdit[idx+1].sourceID))
