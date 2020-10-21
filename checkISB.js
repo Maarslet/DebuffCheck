30c530b6a303f43cae9040685e9963c03b8e394d
Maarslet <12084712+Maarslet@users.noreply.github.com>
2020-10-21 10:14:36 +0200

    Update checkISB.js

--- checkISB.js
+++ checkISB.js
@@ -203,6 +203,7 @@ function checkISB() {
       //remover = damageEdit[timestampList.indexOf(debuffEdit[i].timestamp)];
       remover = damageEdit[timestampList.indexOf(findClosest(debuffEdit[i].timestamp,timestampList))];
       if (i<debuffEdit.length-1 && debuffEdit[i].timestamp==debuffEdit[i+1].timestamp && debuffEdit[i].type == "removedebuff" && debuffEdit[i+1].type == "applydebuff") {}
+      else if (i<debuffEdit.length-1 && debuffEdit[i].type == "removedebuff" && (debuffEdit[i].timestamp+60000)<debuffEdit[i+1].timestamp) {}
       else if (Math.abs(remover.timestamp-debuffEdit[i].timestamp)<25) {
         try {
           who = fightData.friendlies[friendIDs.indexOf(remover.sourceID)];
