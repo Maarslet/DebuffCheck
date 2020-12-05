0fd95382c4f34108508d3141884778588853b2d2
Olli Hiekkaranta <olli.hiekkaranta@gmail.com>
2020-12-05 13:51:48 +0200

    Update target ids for naxx
    * Fix MCd target applying a debuff
    * Fix some boss fights with adds not being recognized

--- debuffAnalyser.js
+++ debuffAnalyser.js
@@ -19,8 +19,8 @@ function processInput() {
 
   var baseURL = "https://classic.warcraftlogs.com:443/v1/report";
   //var filter = "&filter=encounterID%21%3D0"; 
-  //encounterID!=0 AND type in ("applydebuff","removedebuff") AND ability.name not in ("Deep Wound","Fireball","Pyroblast") AND target.id in (12118,11982,12259,12057,12056,12264,12098,11988,11502,10184,12435,13020,12017,11983,14601,11981,14020,11583,14507,14517,14510,11382,15082,14509,15114,14515,11380,14834,15348,15341,15340,15370,15369,15339,15263,15543,15511,15544,15516,15510,15299,15509,15276,15517,15589,15727)
-  var filter="&filter=encounterID%21%3D0%20AND%20type%20in%20%28%22applydebuff%22%2C%22removedebuff%22%29%20AND%20ability.name%20not%20in%20%28%22Deep%20Wound%22%2C%22Fireball%22%2C%22Pyroblast%22%29%20AND%20target.id%20in%20%2812118%2C11982%2C12259%2C12057%2C12056%2C12264%2C12098%2C11988%2C11502%2C10184%2C12435%2C13020%2C12017%2C11983%2C14601%2C11981%2C14020%2C11583%2C14507%2C14517%2C14510%2C11382%2C15082%2C14509%2C15114%2C14515%2C11380%2C14834%2C15348%2C15341%2C15340%2C15370%2C15369%2C15339%2C15263%2C15543%2C15511%2C15544%2C15516%2C15510%2C15299%2C15509%2C15276%2C15517%2C15589%2C15727%29";
+  //encounterID!=0 AND type in ("applydebuff","removedebuff") AND ability.name not in ("Deep Wound","Fireball","Pyroblast") AND target.id in (12118,11982,12259,12057,12056,12264,12098,11988,11502,10184,12435,13020,12017,11983,14601,11981,14020,11583,14507,14517,14510,11382,15082,14509,15114,14515,11380,14834,15348,15341,15340,15370,15369,15339,15263,15543,15511,15544,15516,15510,15299,15509,15276,15517,15589,15727,15956,15953,15952,15954,15936,16011,16061,16060,16064,16065,16062,16063,16028,15931,15932,15928,15989,15990)
+  var filter="&filter=encounterID%21%3D0%20AND%20type%20in%20%28%22applydebuff%22%2C%22removedebuff%22%29%20AND%20ability.name%20not%20in%20%28%22Deep%20Wound%22%2C%22Fireball%22%2C%22Pyroblast%22%29%20AND%20target.id%20in%20%2812118%2C11982%2C12259%2C12057%2C12056%2C12264%2C12098%2C11988%2C11502%2C10184%2C12435%2C13020%2C12017%2C11983%2C14601%2C11981%2C14020%2C11583%2C14507%2C14517%2C14510%2C11382%2C15082%2C14509%2C15114%2C14515%2C11380%2C14834%2C15348%2C15341%2C15340%2C15370%2C15369%2C15339%2C15263%2C15543%2C15511%2C15544%2C15516%2C15510%2C15299%2C15509%2C15276%2C15517%2C15589%2C15727%2C15956%2C15953%2C15952%2C15954%2C15936%2C16011%2C16061%2C16060%2C16064%2C16065%2C16062%2C16063%2C16028%2C15931%2C15932%2C15928%2C15989%2C15990%29";
   try {
     var fightData = new XMLHttpRequest();
     fightData.open("Get", baseURL + "/fights/" + logID + "?" + API.slice(1,API.length), false);
@@ -56,7 +56,6 @@ function processInput() {
     }
     enemyIDs[i] = fightData.enemies[i].id;
   }
-  
   var friendIDs = new Array;
   for (var i=0; i<fightData.friendlies.length; i++) {
     friendIDs[i] = fightData.friendlies[i].id;
@@ -76,10 +75,20 @@ function processInput() {
   for (var i=0; i<bossIDs.length; i++) {
     val = bossIDs[i];
     temp = fightData.enemies[enemyIDs.indexOf(val)].fights;
-    bossNames[count]  = fightData.fights[fightData.enemies[enemyIDs.indexOf(val)].fights[temp.length-1].id-1].name;
-    bossStarts[count] = fightData.fights[fightData.enemies[enemyIDs.indexOf(val)].fights[temp.length-1].id-1].start_time;
-    bossEnds[count]   = fightData.fights[fightData.enemies[enemyIDs.indexOf(val)].fights[temp.length-1].id-1].end_time;
-    count++
+    if (temp.length > 1) { // Handle boss fights with adds better, loatheb fight wasn't being displayed without this
+      for (var j=0; j < temp.length; j++) {
+        bossNames[count]  = fightData.fights[fightData.enemies[enemyIDs.indexOf(val)].fights[j].id-1].name;
+        bossStarts[count] = fightData.fights[fightData.enemies[enemyIDs.indexOf(val)].fights[j].id-1].start_time;
+        bossEnds[count]   = fightData.fights[fightData.enemies[enemyIDs.indexOf(val)].fights[j].id-1].end_time;
+        count++
+      }
+    }
+    else {
+      bossNames[count]  = fightData.fights[fightData.enemies[enemyIDs.indexOf(val)].fights[temp.length-1].id-1].name;
+      bossStarts[count] = fightData.fights[fightData.enemies[enemyIDs.indexOf(val)].fights[temp.length-1].id-1].start_time;
+      bossEnds[count]   = fightData.fights[fightData.enemies[enemyIDs.indexOf(val)].fights[temp.length-1].id-1].end_time;
+      count++
+    }
   }
   
   var debuffEdit = debuffData.events; 
